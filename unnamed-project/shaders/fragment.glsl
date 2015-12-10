6c1af2740c433211b5da32dc506c0927e17ebe4a
Tom Kneiphof <tomkneiphof@gmail.com>
2015-12-10 12:01:38 +0100

    Division by zero!

--- unnamed-project/shaders/fragment.glsl
+++ unnamed-project/shaders/fragment.glsl
@@ -26,6 +26,8 @@ float cdot(vec3 a, vec3 b)
 
 float beckmannDistribution(float dotNH)
 {
+    if (dotNH == 0.)
+        return 0.;
     float sigma2 = roughness * roughness;
     float m = 2.;
     float alpha = acos(dotNH);
@@ -43,23 +45,28 @@ float schlickApprox(float dotVH, float n1, float n2)
 
 float geometricAttenuation(float dotNH, float dotVN, float dotVH, float dotNL)
 {
+    if (dotVH == 0.)
+        return 0.;
     float g1 = 2.*dotNH*dotVN/dotVH;
     float g2 = 2.*dotNH*dotNL/dotVH;
     return min(1., min(g1, g2));
 }
 
-float cooktorranceTerm(vec3 normal, vec3 light)
+float cooktorranceTerm(vec3 v, vec3 n, vec3 l)
 {
     // Im eye space ist die Richtung zum Betrachter schlicht die Z-Achse
     // TODO are we in "eye" or "camera"
-    vec3 view = vec3(0.0, 0.0, 1.0);
-    vec3 halfViewLight = normalize(light + view);
+    // vec3 v = vec3(0.0, 0.0, 1.0);
+    vec3 h = normalize(l + v);
 
     // Precompute scalar products
-    float dotVN = cdot(view, normal);
-    float dotNL = cdot(normal, light);
-    float dotNH = cdot(normal, halfViewLight);
-    float dotVH = cdot(view, halfViewLight);
+    float dotVN = cdot(v, n);
+    float dotNL = cdot(n, l);
+    float dotNH = cdot(n, h);
+    float dotVH = cdot(v, h);
+
+    if (dotNL == 0. || dotVN == 0.)
+        return 0.;
 
     float D = beckmannDistribution(dotNH);
     float F = schlickApprox(dotVH, 1.0, refractionIndex);
@@ -72,11 +79,11 @@ void main()
 {
     vec3 n = normalize(normal);
     vec3 l = normalize(-lightDirection);
-
+    vec3 v = normalize(-world); // eye is in the origin.
     float diffuseTerm = cdot(n, l);
 
-    float specularTerm = cooktorranceTerm(n, l);
+    float specularTerm = cooktorranceTerm(v, n, l);
 
     fragColor = vec4(clamp(specularTerm * specularColor + diffuseTerm * diffuseColor + ambientColor, 0., 1.), 1.);
-    // fragColor = vec4(n, 1.);
+    // fragColor = vec4(l, 1.);
 }
