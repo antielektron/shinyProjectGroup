568921c3fdf2483ffb2b86754c3b79fa8f1ee545
Jonas Weinz <s6jowein@uni-bonn.de>
2015-12-30 21:26:12 +0100

    added first game logic stuff

---
+++ unnamed-project/src/GameLogic/FiniteStateMachine.cpp
@@ -0,0 +1,43 @@
+#include "FiniteStateMachine.h"
+
+//------------------------------------------------------------------------------
+FiniteStateMachine::FiniteStateMachine() :
+    FiniteStateMachine(std::unique_ptr<State>(new State("start")))
+{
+
+}
+
+//------------------------------------------------------------------------------
+FiniteStateMachine::FiniteStateMachine(std::unique_ptr<State> startState)
+{
+    m_stateMap[startState->getName()] = std::move(startState);
+    m_startState = startState.get();
+}
+
+//------------------------------------------------------------------------------
+FiniteStateMachine::~FiniteStateMachine()
+{
+    // nothing to do here
+}
+
+//------------------------------------------------------------------------------
+State *FiniteStateMachine::addState(std::unique_ptr<State> state)
+{
+    m_stateMap[state->getName()] = std::move(state);
+}
+
+//------------------------------------------------------------------------------
+void FiniteStateMachine::addTransition(std::string srcStateName,
+                                       std::string dstStateName,
+                                       std::string inputName)
+{
+    m_stateMap[srcStateName]->addTransition(inputName,
+                                            m_stateMap[dstStateName].get());
+}
+
+//------------------------------------------------------------------------------
+void FiniteStateMachine::addEpsilonTransition(std::string srcStateName,
+                                              std::string dstStateName)
+{
+    m_stateMap[srcStateName]->setEpsilonTransition(m_stateMap[dstStateName].get());
+}
---
+++ unnamed-project/src/GameLogic/State.h
@@ -0,0 +1,43 @@
+#ifndef STATE_H
+#define STATE_H
+
+#include <map>
+#include <string>
+#include <QObject>
+#include "smartiterator.h"
+
+class State
+{
+    Q_OBJECT
+public:
+    State(std::string name, State *defaultNextState = nullptr, bool isFinalState = false);
+    virtual ~State();
+
+    typedef std::map<std::string, State *>::iterator TransitionMapType;
+
+    void addTransition(std::string input, State *nextState);
+    void setEpsilonTransition(State *nextState);
+    State *getNextState(std::string input);
+    State *getEpsilonState();
+    void setFinalState(bool isFinalState);
+    bool isFinalState();
+    range<TransitionMapType> getTransitions();
+    const std::string &getName();
+
+    virtual void performAction();
+
+signals:
+    void stateEntered(State *state);
+    void stateLeaved(State *state);
+
+public slots:
+    void action();
+
+protected:
+    std::map<std::string, State *> m_stateTransitions;
+    State *m_defaultNextState; //<** for epsilon transition
+    bool m_isFinalState;
+    std::string m_name;
+};
+
+#endif // STATE_H
