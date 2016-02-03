7a93093a13c85b925871e19d267612994d0bc3db
Jonas Weinz <s6jowein@uni-bonn.de>
2016-02-03 20:19:24 +0100

    hear vader marching in front of you (as audio test)

--- unnamed-project/src/utility.cpp
+++ unnamed-project/src/utility.cpp
@@ -16,10 +16,28 @@ void playAudioSampleFromFile(ALbyte *wavfile)
 {
 
     ALuint helloBuffer, helloSource;
-    helloBuffer = alutCreateBufferHelloWorld();
+    helloBuffer = alutCreateBufferFromFile("sounds/imperial_march.wav");
+    if (helloBuffer == -1)
+    {
+        QMessageBox::critical(nullptr, "ERROR", " could not load audio file");
+    }
     alGenSources(1, &helloSource);
     alSourcei(helloSource, AL_BUFFER, helloBuffer);
     alSourcePlay(helloSource);
+    alSource3f(helloSource, AL_POSITION, -1.0, 0.0, 0.0);
+    for (int i = 0; i < 10; i++)
+    {
+        for (float j = -5.0; j < 5.0; j+= 0.5)
+        {
+            alSource3f(helloSource, AL_POSITION, j, 5.0, 0.0);
+            alutSleep(0.1);
+        }
+        for (float j = 5.0; j > -5.0; j -= 0.5)
+        {
+            alSource3f(helloSource, AL_POSITION, j, 5.0, 0.0);
+            alutSleep(0.1);
+        }
+    }
     alutSleep(1);
 
 }
