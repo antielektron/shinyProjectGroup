84ced18ac5b75f8b429ff0f9b816e085954bdc0a
Tom Kneiphof <tomkneiphof@gmail.com>
2016-02-21 14:27:14 +0100

    Worked around nvidia quirks

--- unnamed-project/shaders/sdsm/create_cascade_views.glsl
+++ unnamed-project/shaders/sdsm/create_cascade_views.glsl
@@ -12,32 +12,37 @@ uniform mat4 lightViewMatrix;
 
 layout (local_size_x = 4) in;
 
-shared vec3 minCorners[4];
-shared vec3 maxCorners[4];
-
 void main()
 {
-    int index = int(gl_GlobalInvocationID.x);
+    // TODO why nvidia only likes it this way
+    vec3 minCorners[4];
+    vec3 maxCorners[4];
 
     // 6 layer
     // minx miny minz maxx maxy maxz
 
-    if (index < 3)
+    for (int index = 0; index < 3; index++)
     {
         vec4 minValue = imageLoad(inputTex, ivec2(0, index));
         minValue = minValue * 2 - 1;
+
         minCorners[0][index] = minValue[0];
         minCorners[1][index] = minValue[1];
         minCorners[2][index] = minValue[2];
         minCorners[3][index] = minValue[3];
 
+
         vec4 maxValue = imageLoad(inputTex, ivec2(0, index + 3));
         maxValue = maxValue * -2 + 1;
+
         maxCorners[0][index] = maxValue[0];
         maxCorners[1][index] = maxValue[1];
         maxCorners[2][index] = maxValue[2];
         maxCorners[3][index] = maxValue[3];
+
     }
+
+    int index = int(gl_GlobalInvocationID.x);
 
     // only very few threads ;) ie 4
     // barrier();
